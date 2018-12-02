1bd3fd29fbbf76d14b3101b4529b19676adc0257
vvhoang <vvhoang@mgmgerrit.mgm-tp.com>
2018-12-02 22:25:39 +0700

    receive data model

---
+++ app/src/main/java/com/example/admin/nav/model/ReceiveImageData.java
@@ -0,0 +1,22 @@
+package com.example.admin.nav.model;
+
+public class ReceiveImageData {
+    private String name;
+    private String body;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
